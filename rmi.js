6aaa899540023d408cf2c4c530a3641afe61cdc6
Niko Uphoff <n.uphoff@sinnerschrader.de>
2011-03-15 18:28:14 +0100

    added an option to deliver and execute non rmi-based objects/callbacks (e.g. for nativ iteration)

--- rmi.js
+++ rmi.js
@@ -1,206 +1,164 @@
+RMI = function(cb, prefix) {
+  this.__Handles = new this.HandleManager(prefix || "rmi");
+  this.__Callback = cb;
+};
 
-
-var BaseTest = function() {
-  this.args = []
-  for(var i = arguments.length-1; i >= 0; --i) {
-    this.args[i] = arguments[i]
-  }
-  this.level = arguments.length
-}
-
-BaseTest.prototype.getLevel = function() {
-  var self = this
-  return function() { return self.level }
-}
-
-/*
-BaseTest.prototype.createDB = function(database, call) {
-  call()
-  return new function(database){
-    this.database = database
-    this.save = function(data, cb) {
-      cb(data+data)
-    }
-  }(database)
-}
-
-*/
-var HandleManager = function(name) {
-  this.refs = {}
-  this.handle = 1;
-  this.addRef = function(ref, id) {
-    if (id) {
-      this.refs[id] = ref
-      return id
-    }
-    for(var handle in this.refs) { 
-      if (this.refs[handle] === ref) { return handle; }
-    }
-    id = "$"+name+"."+(this.handle++)+"$"
-    this.refs[id] = ref
-    return id;
-  }
-  this.getRef = function(handle) {
-    return this.refs[handle]
-  }
-  this.delHandle = function(handle) {
-    delete this.refs[handle]
-  }
-}
-
-var RMIServerHandles = new HandleManager('srv')
-var RMIClientHandles = new HandleManager('clt')
-
-RMI ={}
-RMI.once = function(fn) {
-  return fn
-}
-                    
-
-asRMI = function(handles) {
-  return function() {
-    var recurse = function(val) {
-      if (typeof(val) == 'function') {
-          return { __RmiFunction__: handles.addRef(val) }
-      } else if (!!(val && val.concat && val.unshift && !val.callee) || !!(val && val.callee)) {
-        /* isArray or isArguments */
-        return function(val) {
-          var dst = []
-          for(var l = val.length-1; l >= 0; --l) {
-            dst[l] = recurse(val[l]) 
-          }
-          return dst
-        }(val)
-      } else if (typeof(val) == 'object') {
-        return function(val) {
-          dst = { __RmiObject__: handles.addRef(val) } 
-          for(var key in val) {
-            if (key == 'asRMI') { continue; }
-            dst[key] = recurse(val[key])
-          }
-          return dst
-        }(val)
-      } 
-      return val
-    }
-    return recurse(this)
-  }
-}
-
-var toRMI = function(handler, invoked) {
-  return function (key, value) {
-    if (key == "__RmiObject__") {
-      handler.addRef(this, value)
-    } else if (value && value.__RmiFunction__) {
-      return function() { 
-        var cbs = []
-        var got_ret = false
-        var return_value = function(value) { 
-console.log('RETURN_VALUE:'+JSON.stringify(value)+":"+ret.__RmiFunction__)
-          handler.delHandle(ret.__RmiFunction__)
-          for(var l = cbs.length-1; l >= 0; --l) { cbs[l](value) }
-          got_ret = true
-          return_value = value
-        }
-        var ret = asRMI(handler).apply(return_value)
-        invoked({
-                  obj: this.__RmiObject__, 
-                  func: value.__RmiFunction__, 
-                  args: asRMI(handler).apply(arguments), 
-                  ret:  ret
-                }) 
-        return {
-                 value: function(cb) { 
-                  if (got_ret) { cb(return_value) }
-                  else { cbs.push(cb)  }
-                 }
-               }
+RMI.prototype = {
+  HandleManager: function(name) {
+    this.refs = {};
+    this.handle = parseInt(Math.random()*(100000),10);
+    this.addRef = function(ref, opts) {
+      if (opts) {
+        this.refs[opts.id] = ref;
+        return opts;
       }
+      
+      for(var handle in this.refs) { 
+        if (this.refs[handle] === ref) { return handle; }
+      }
+      
+      id = "$" + name + "." + (this.handle++) + "$";
+      this.refs[id] = ref;
+      opts || (opts = {});
+      opts.id = id;
+      return opts;
+    };
+    
+    this.getRef = function(handle) {
+      return this.refs[handle];
+    };
+    
+    this.delHandle = function(handle) {
+      delete this.refs[handle];
+    };
+  },
+  
+  as: function(handles, type, params) {
+    return function() {
+      var recurse = function(val, opts) {
+        if (typeof(val) == "function") {
+          opts = {};
+          opts["__" + (type || "RmiFunction") + "__"] = handles.addRef(val, params);
+          return opts;
+        } else if (!!(val && val.concat && val.unshift && !val.callee) || !!(val && val.callee)) {
+          /* isArray or isArguments */
+          return function(val, l, dst) {
+            dst = [];
+            for (l = val.length-1; l >= 0; --l) {
+              dst[l] = recurse(val[l]); 
+            }
+            return dst;
+          }(val);
+        } else if (typeof(val) == "object") {
+          return function(val, key, dst) {
+            dst = { __RmiObject__: handles.addRef(val, params) };
+            for (key in val) {
+              if (key == "asRMI") { continue; }
+              dst[key] = recurse(val[key]);
+            }
+            return dst;
+          }(val);
+        } else {
+          return val;
+        }
+      };
+      return recurse(this);
+    };
+  },
+  
+  to: function(handler, RMI) {
+    RMI = this;
+    
+    return function (key, value) {
+      var callback = function() {
+        var called_arguments = arguments;
+        var called_instance = this;
+        
+        return {
+          call: function(cb, params) {
+            var got_ret = false;
+            var opts = {
+              obj: called_instance.__RmiObject__,
+              func: value.__RmiFunction__ || value.__RmiCallback__,
+              args: (params && params.plain) ? (function() { called_arguments.plain = true; return called_arguments })() : RMI.as(handler).apply(called_arguments)
+            };
+            
+            if (cb) {
+              var return_value = function(value) {
+                handler.delHandle(ret.__RmiFunction__||ret.__RmiCallback__);
+                
+                cb(value);
+                got_ret = true;
+                return_value = value;
+              };
+              
+              var ret = RMI.as(handler).apply(return_value);
+              
+              opts.ret = ret;
+            }
+            
+            RMI.__Callback(opts);
+            if (cb) {
+              if (got_ret) {
+                cb(return_value);
+              }
+            }
+          }
+        };
+      };
+      
+      if (key == "__RmiObject__") {
+        handler.addRef(this, {id: value});
+      } else if (value && value.__RmiCallback__) {
+        return function() {
+          callback.apply({}, arguments).call(null, value.__RmiCallback__);
+        };
+      } else if (value && value.__RmiFunction__) {
+        return callback;
+      }
+      return value;
+    };
+  },
+  
+  server: function(obj) {
+    obj.asRMI = this.as(this.__Handles);
+    return obj;
+  },
+  
+  client: function(str) {
+    var RMI = this,
+        ret = JSON.parse(str, this.to(this.__Handles));
+    
+    ret.cb = function(fn) {
+      return RMI.as(RMI.__Handles, "RmiCallback").apply(fn);
+    };
+    
+    ret.cb.plain = function(fn) {
+      return RMI.as(RMI.__Handles, "RmiCallback", {plain: true}).apply(fn);
+    };
+    
+    return ret;
+  },
+  
+  invoke: function(str) {
+    var invocation = JSON.parse(str, this.to(this.__Handles)),
+        obj = invocation.obj ? this.__Handles.getRef(invocation.obj.id) : undefined,
+        func = this.__Handles.getRef(invocation.func.id),
+        args;
+    
+    if(invocation.args.plain) {
+      delete invocation.args.plain;
+      
+      args = [];
+      for (var i in invocation.args) {
+        args[i] = invocation.args[i];
+      }
+    } else {
+      args = invocation.args;
     }
-    return value;
+    
+    ret = func.apply(obj, args);
+    
+    invocation.ret && invocation.ret(ret).call();
   }
-}
-
-var RMIServer = {
-  build: function(obj) {
-   obj.asRMI = asRMI(RMIServerHandles);
-   return obj;
-  },
-  invoke: function(str) {
-    var invocation = JSON.parse(str, toRMI(RMIServerHandles, function(obj) {
-      RMIClient.invoke(JSON.stringify(obj))
-    })) 
-    //console.log('RMIServer.Invocation:'+str)
-    var obj = RMIServerHandles.getRef(invocation.obj)
-    var func = RMIServerHandles.getRef(invocation.func)
-    //console.log("obj:"+obj)
-    //console.log("func:"+func)
-    invocation.ret(func.apply(obj, invocation.args))
-  }
-}
-
-var RMIClient = {
-  build: function(str, invoked) {
-    return JSON.parse(str, toRMI(RMIClientHandles, invoked))
-  },
-  invoke: function(str) {
-    //console.log('RMIClient:invoke:'+str)
-    var invocation = JSON.parse(str, toRMI(RMIClientHandles, function(obj) {
-      RMIServer.invoke(JSON.stringify(obj))
-    }))
-    var obj = RMIClientHandles.getRef(invocation.obj) || {}
-    var func = RMIClientHandles.getRef(invocation.func)
-    //console.log("obj:"+obj)
-    //console.log("func:"+func)
-    func.apply(obj, invocation.args)
-  }
-}
-
-var srv = RMIServer.build(new BaseTest('meno'))
-
-json = JSON.stringify(srv.asRMI())
-
-var clt = RMIClient.build(json, function(obj) {
-  var json = JSON.stringify(obj)
-//console.log('client:invocation:'+json)
-  return RMIServer.invoke(json)
-})
-
-/*
-var ret = clt.createDB('meno', RMI.once(function() {
-  console.log('createDB:callback')
-}))
-ret.value(function(value) {
-  console.log('createDB:return:'+JSON.stringify(value))
-  value.save('meno', RMI.once(function(done) {
-    console.log('createDB:save:'+done)
-  }))
-})
-
-*/
-
-for(var i = 0; i < 1; ++i) {
-ret = clt.getLevel()
-
-ret.value(function(value) {
-  value().value(function(value) { 
- //   console.log('f-callback:'+value) 
-  }) 
-})
-
-if (!(i % 1000)) {
-  var j = 0
-  var k = 0
-  for(var ref in RMIClientHandles.refs) { ++j }
-  for(var ref in RMIServerHandles.refs) { 
-   console.log(ref,  RMIServerHandles.refs[ref])
-   ++k 
-  }
-  console.log('C:'+j+":S:"+k)
-  console.log('C:'+JSON.stringify(RMIClientHandles.refs))
-  console.log("S:"+JSON.stringify(RMIServerHandles.refs))
-}
-}
-
-
-
+};
